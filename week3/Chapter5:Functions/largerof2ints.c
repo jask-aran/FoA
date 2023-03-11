e390b04d469af76b62c4446a67c312522d40efc0
Jaskaran Singh <jaskarann.singhh@gmail.com>
2023-03-11 12:38:01 +1100

    itn programs

---
+++ week3/Chapter5:Functions/largerof2ints.c
@@ -0,0 +1,38 @@
+/* Exercise 5.01: Find the larger of two integers
+ * Scaffold: Jianzhong Qi, March 2013
+ * (c) University of Melbourne */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* function prototypes */
+
+int max_2_ints(int n1, int n2);
+
+int
+main(int argc, char *argv[]) {
+
+    /* get two integers */
+    printf("Enter two integers: ");
+    int n1, n2;
+    if (scanf("%d %d", &n1, &n2) != 2) {
+        // printf("Error in input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* scaffolding -- get the larger integer */
+    int max = max_2_ints(n1, n2);
+    printf("max_2_ints(%d, %d) = %d\n", n1, n2, max);
+
+    return 0;
+}
+
+/* return the larger between two integers n1 and n2 */
+int
+max_2_ints(int n1, int n2) {
+    if (n1 > n2) {
+        return n1;
+    } else {
+        return n2;
+    }
+}
