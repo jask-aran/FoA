6cc23b07accebf58bd61fbeefa1d2911a88662a8
Jaskaran Singh <jaskarann.singhh@gmail.com>
2023-03-11 12:58:09 +1100

    find the result of a power operation, handles int overflow

--- week3/Chapter5:Functions/intpower.c
+++ week3/Chapter5:Functions/intpower.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
 int main(void) {
-    int base = 3, exponent = 4;
+    int base = 2, exponent = 32;
     printf("Enter the base and the exponent: ");
     scanf("%d %d", &base, &exponent);
 
     if (exponent == 0) {
         printf("%d^%d = 1\n", base, exponent);
     } else {
-        int result = base;
+        int running = base;
 
         for (int i = 1; i < exponent; i++) {
-        result = result * base;
+
+            if (running * base <= 0) {
+                printf("Error: Integer overflow when attempting multiplication\n");
+                exit(EXIT_FAILURE);
+            } else {
+                running = running * base;
+            }          
         }
-        
-        printf("%d^%d = %d\n", base, exponent, result); 
+        printf("%d^%d = %d\n", base, exponent, running); 
+        // printf("%d", INT_MAX);
     }  
 }
