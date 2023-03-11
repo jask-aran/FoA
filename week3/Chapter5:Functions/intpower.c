8e4736251b94cd4f207f05c973dbb89a324cfd78
Jaskaran Singh <jaskarann.singhh@gmail.com>
2023-03-11 12:37:55 +1100

    working intpower doesnt handle overflow

--- week3/Chapter5:Functions/intpower.c
+++ week3/Chapter5:Functions/intpower.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(void) {
     int base = 3, exponent = 4;
     printf("Enter the base and the exponent: ");
     scanf("%d %d", &base, &exponent);
 
-    int result = base;
-    for (int i = 1; i < exponent; i++) {
-        result = result * base;
-    }
-
     if (exponent == 0) {
         printf("%d^%d = 1\n", base, exponent);
     } else {
+        int result = base;
+
+        for (int i = 1; i < exponent; i++) {
+        result = result * base;
+        }
+        
         printf("%d^%d = %d\n", base, exponent, result); 
-    }   
+    }  
 }
